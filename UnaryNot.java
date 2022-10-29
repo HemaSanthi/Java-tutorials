72f99c7b7566137f81b752a464904fecef7da853
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-10-29 13:17:21 +0530

    Create UnaryNot.java

---
+++ UnaryNot.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class UnaryNot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a= true;
+		System.out.println(!a);
+		
+
+	}
+
+}
