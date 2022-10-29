b9cec2f3420309c6d641b95cb6ff42f4bd577aeb
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-10-29 13:16:41 +0530

    Create TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=6, b=10;
+	    String res=(a==5)? "equal": "not equal";
+		System.out.println(res);
+		res=(b==10)? "equal": "not equal";
+		System.out.println(res);
+
+	}
+
+}
