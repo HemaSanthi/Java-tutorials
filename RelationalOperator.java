456e4df442b70ddf5e0a9af54d3e5aa5d58e53de
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-10-29 13:15:48 +0530

    Create RelationalOperator.java

---
+++ RelationalOperator.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=8; 	
+		boolean e=a>b;
+		System.out.println("The Value Of a "+ a+" b "+b+" respectively");
+		System.out.println("The Value Of a>b " + e);
+		e=a<b;
+		System.out.println("The Value Of a<b " + e);
+		e=(a<=b);
+		System.out.println("The Value Of a<=b " + e);
+		e=(a>=b);
+		System.out.println("The Value Of a>=b " + e);
+		e=(a<=b);
+		System.out.println("The Value Of a<=b " + e);
+		e=(a==b);
+		System.out.println("The Value Of a==b " + e);
+		e=(a!=b);
+		System.out.println("The Value Of a!=b " + e);
+
+
+	}
+
+}
