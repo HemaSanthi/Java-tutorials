9ca9c95ba42022ce7958dad3c69cefe972d38df4
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-10-29 12:54:59 +0530

    Create assignment.java

---
+++ assignment.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10, num2=15;
+		num1+=5;
+		System.out.println("The Output Of num1+5 " + num1);
+		num1-=5;
+		System.out.println("The Output Of num1-= " + num1);
+		num1*=5;
+		System.out.println("The Output Of num1*=5 " + num1);
+		num1/=5;
+		System.out.println("The Output Of num1/=5 " + num1);
+		num2%=2;
+		System.out.println("The Output Of num2%=5 " + num2);
+
+	}
