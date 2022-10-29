f697b923583300caae93f61f6105db866e2190bb
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-10-29 13:08:29 +0530

    Create logicalOr.java

---
+++ logicalOr.java
@@ -0,0 +1,23 @@
+package sample;
+import java.util.Scanner;
+
+public class logicalOr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value;
+		Scanner sc= new Scanner (System.in);
+		System.out.println("Enter the Value");
+		value= sc.nextInt();
+		if ((value>=10)&& (value%2==0))
+		{
+			System.out.println("Its true");
+		}
+		else
+		{
+			System.out.println("Its False");
+		}
+
+	}
+
+}
