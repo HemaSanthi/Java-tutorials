94d923fe844daee3d5205c1deaafd16885b1c3fa
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-10-29 13:04:55 +0530

    Create IncrementOverride.java

---
+++ IncrementOverride.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class IncrementOverride {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b,c;
+		b=a++;
+		c=++a;
+		System.out.println("a:"+a+"b:"+b);
+		System.out.println("a:"+a+"C:"+c);
+
+	}
+
+}
