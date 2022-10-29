bc6405f230a3b4df46942a822e97d16d2b74fd8d
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-10-29 12:52:21 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=5, num2=8;
+		int sum, sub, mul, div,mod;
+		sum=num1+num2;
+		sub=num1-num2;
+		mul=num1*num2;
+		div=num2/num1;
+		mod=num2%num1;
+		System.out.println("sum"+ sum);
+		System.out.println("sub"+ sub);
+		System.out.println("mul"+ mul);
+		System.out.println("div"+ div);
+		System.out.println("mod"+ mod);
+
+	}
+
+}
