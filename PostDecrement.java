1ff4484ec016843664804dedd9701a35fd543568
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-10-29 13:09:27 +0530

    Create PostDecrement.java

---
+++ PostDecrement.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class PostDecrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b;
+		b=--a;
+		System.out.println("a="+ a + "b="+ b);
+		
+
+	}
+
+}
