3ac2e54f7305c42a6b88eca73dc653915a6d348b
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-10-29 13:06:13 +0530

    Create InstanceOfOperator.java

---
+++ InstanceOfOperator.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class InstanceOfOperator {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InstanceOfOperator a=new InstanceOfOperator();
+		System.out.println(a instanceof InstanceOfOperator);
+	}
+
+}
