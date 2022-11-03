f2c392341222da7afc0d8b52f837f2a7471982cc
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-11-03 12:26:16 +0530

    Create AritmeticOperation1.java

---
+++ AritmeticOperation1.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class AritmeticOperation1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int res=((((8+2345)/3)%5)*5);
+		System.out.println(res);
+		
+		
+
+	}
+
+}
